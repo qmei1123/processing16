b65cb665041aad6977a04587c4f524a2e66721e0
qmei1123 <qmei@bu.edu>
2018-02-27 13:47:09 -0500

    screen

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
-var xspacing = 16;    // Distance between each horizontal location
+var xspacing = 5;    // Distance between each horizontal location
 var w;                // Width of entire wave
 var theta = 0.0;      // Start angle at 0
-var amplitude = 75.0; // Height of wave
+var amplitude = 150.0; // Height of wave
 var period = 100.0;   // How many pixels before the wave repeats
 var dx;               // Value for incrementing x
 var yvalues;  // Using an array to store height values for the wave
 
 function setup() {
-  createCanvas(1000, 900);
-  w = width+16;
+     createCanvas(window.innerWidth, window.innerHeight);
+  w = width;
   dx = (TWO_PI / period) * xspacing;
   yvalues = new Array(floor(w/xspacing));
 }
@@ -22,7 +22,7 @@ function draw() {
 function calcWave() {
   // Increment theta (try different values for 
   // 'angular velocity' here)
-  theta += 0.005;
+  theta += 0.0075;
 
   // For every x value, calculate a y value with sine function
   var x = theta;
@@ -36,8 +36,8 @@ function renderWave() {
   noStroke();
   // A simple way to draw the wave with an ellipse at each location
 for (var y = -150; y < height*2; y+=20) {
-      for (var x = 0; x < yvalues.length; x++) {
-            rect(x*xspacing, y/2+yvalues[x], 5, 5);
+      for (var x = 20; x < 40; x++) {
+            rect(x*xspacing, y/2+yvalues[x], 3, 3);
             fill(random(0, 75));
 
       }
